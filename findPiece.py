5cc781575e863c14c8c44a457d7326889e023af7
Mekyle Hemani <153373890+Mekyle-Hemani@users.noreply.github.com>
2024-06-28 17:12:55 -0400

    Added edge finding to pieces
    Let the find function find edge colors as well

--- findPiece.py
+++ findPiece.py
@@ -1,17 +1,27 @@
 def find(type, colour, scramble, secondarycolour=None, tertiarycolour=None, confirm=0):
     locations = []
-    neighbouring = [[3, 4, 2, 1],[5, 2, 4, 0],[3, 0, 5, 1],[5, 4, 2, 0],[3, 5, 0, 1],[3, 2, 4, 1]]
+    neighbouring = [
+        [3, 4, 2, 1], #White
+        [5, 2, 4, 0], #Orange
+        [3, 0, 5, 1], #Green
+        [5, 4, 2, 0], #Red
+        [3, 5, 0, 1], #Blue
+        [3, 2, 4, 1] #Yellow
+    ]
     for i in range(6):
         for j in range(9):
             if (scramble[i][j] == colour):
                 if ((type == 0)and(j%2 == 1)): #Edge
-                    if (secondarycolour==None):
-                        locations.append([i, j])
-                    else:
-                        pass
+                    locations.append([i, len(locations)])        
                 elif (((type == 1)and(j%2 == 0))and(j!=4)): #Corner
                     if (secondarycolour==None):
                         locations.append([i, j])
+    
+    if ((type == 0)and(secondarycolour!=None)): #Edge and second colour is applied
+        for i in range(4):
+            if (neighbouring[locations[i][0]][locations[i][1]] == secondarycolour):
+                return [[locations[i][0], locations[i][1]], [neighbouring[locations[i][0]][locations[i][1]], neighbouring[neighbouring[locations[i][0]][locations[i][1]]][locations[i][0]]]]
+
     if (confirm == 1):
         if (len(locations)>4):
             print(f"Too many matches in cube for type={type}, color={colour}, secondarycolour={secondarycolour}, tertiarycolour={tertiarycolour}")
