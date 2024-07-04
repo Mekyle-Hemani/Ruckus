a4827d97374db791a390c8b0b7afd4da27114dde
Mekyle Hemani <153373890+Mekyle-Hemani@users.noreply.github.com>
2024-07-04 11:53:05 -0400

    Update arduinoConnection.py
    Made it so that the code shuts itself down after sending a string and returns if it was successful or not

--- arduinoConnection.py
+++ arduinoConnection.py
@@ -1,38 +1,44 @@
 import serial
 import serial.tools.list_ports
 
-def senddata(data, rate=9600):
+def senddata(data, rate=9600, debug=1):
     ports = serial.tools.list_ports.comports()
     comports = []
     for port in ports:
         comports.append(port.device)
 
-    port = comports[0]
-
     if (len(comports) == 0):
-        print("Please ensure that the Ruckus Controller is connected")
+        if (debug == 1):
+            print("Please ensure that the Ruckus Controller is connected")
+        return False
 
     else:
         port = comports[0]
         try:
             ser = serial.Serial(port, baudrate=rate, timeout=1)
-            print(f"Connected to {port}")
-
-            while True:
-                ser.write(f"{data}\n".encode('utf-8'))
+            if (debug == 1):
+                print(f"Connected to {port}")
 
             ser.write(f"{data}\n".encode('utf-8'))
 
+            if (debug == 1):
+                print(f"Sent '{data}' succesfully")
+
             #response = ser.readline().decode('utf-8').strip()
 
+            return True
+
         except serial.SerialException as e:
-            print(f"Failed to connect to {port}: {e}")
-            senddata(data, rate=rate)
+            if (debug == 1):
+                print(f"Failed to connect to {port}: {e}")
+            return False
         except KeyboardInterrupt:
-            print("Stopping communication...")
+            if (debug == 1):
+                print("Stopping communication...")
+            return False
         finally:
             if ser.is_open:
                 ser.close()
 
 if __name__ == "__main__":
-    senddata("", 9600)
+    print(senddata("hi", 9600))
