8408f24c3d7c01d1b34e59124e82b0ffe2baeafa
Mekyle Hemani <153373890+Mekyle-Hemani@users.noreply.github.com>
2024-07-05 12:47:08 -0400

    Create RuckusController.ino
    This is the controller code that the Ruckus Controller will be using to rotate all the motors off of a given move-set from the python-running device

---
+++ RuckusController/RuckusController.ino
@@ -0,0 +1,14 @@
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    String data = Serial.readString();
+    if (data.indexOf("hi") >= 0) {
+      digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println("Received: " + data);
+  }
+}
