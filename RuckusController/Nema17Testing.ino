1102f7e509b3ffe9d2e0420fc5dbb575a5fbf13a
Mekyle Hemani <153373890+Mekyle-Hemani@users.noreply.github.com>
2024-07-05 19:29:41 -0400

    Create Nema17Testing.ino
    Added a script that can be used to test the functionality of the drivers

---
+++ RuckusController/Nema17Testing.ino
@@ -0,0 +1,34 @@
+const int dirPin = 8;
+const int stepPin = 7;
+const int stepsPerRevolution = 200;
+
+void setup()
+{
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+}
+void loop()
+{
+  digitalWrite(dirPin, HIGH);
+
+  for(int x = 0; x < stepsPerRevolution; x++)
+  {
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(500);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(500);
+  }
+  delay(500);
+
+  spin(200, 1, 400); 
+}
+
+void spin(int count, int dir, int del){
+  digitalWrite(dirPin, dir);
+  for (int i=0; i<count; i++){
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(del);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(del);
+  }
+}
