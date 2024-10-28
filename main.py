d38172842a5fb75234c69887b92165ad0206891f
Mekyle Hemani <153373890+Mekyle-Hemani@users.noreply.github.com>
2024-10-28 13:54:28 -0400

    Update main.py
    Added communication establishment

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import SimpleSerial.SimpleSerialMain
 import grabScramble
 import solve
 import SimpleSerial
@@ -5,3 +6,13 @@ import SimpleSerial
 scramble = grabScramble.grabScramble()
 
 print(solve.solve(scramble))
+
+
+#Establishing communication with microcontroller
+while True:
+    if "testing" in (SimpleSerial.SimpleSerialMain.readDataOnce):
+        break
+while True:
+    SimpleSerial.SimpleSerialMain.writeDataOnce("sending")
+    if "recieved" in (SimpleSerial.SimpleSerialMain.readDataOnce):
+        break
