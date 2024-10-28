2a0c56dfa9622c771479c5189b1642192a9debf0
Mekyle Hemani <153373890+Mekyle-Hemani@users.noreply.github.com>
2024-10-28 14:08:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,28 @@ import SimpleSerial.SimpleSerialMain
 import grabScramble
 import solve
 import SimpleSerial
+import colourprint
 
 scramble = grabScramble.grabScramble()
 
-print(solve.solve(scramble))
+solution = solve.solve(scramble)
 
+colourprint.print_colored(solution, colourprint.GREEN)
 
 #Establishing communication with microcontroller
 while True:
-    if "testing" in (SimpleSerial.SimpleSerialMain.readDataOnce):
+    if "testing" in (SimpleSerial.SimpleSerialMain.readDataOnce()):
         break
 while True:
     SimpleSerial.SimpleSerialMain.writeDataOnce("sending")
-    if "recieved" in (SimpleSerial.SimpleSerialMain.readDataOnce):
+    if "recieved" in (SimpleSerial.SimpleSerialMain.readDataOnce()):
         break
+colourprint.print_colored("Communication established", colourprint.BLUE)
+
+SimpleSerial.SimpleSerialMain.writeDataOnce(solution)
+
+while True:
+    if "testing" in (SimpleSerial.SimpleSerialMain.readDataOnce()):
+        break
+
+colourprint.print_colored(("Cube solved using "+solution), colourprint.GREEN)
