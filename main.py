1b30278d855e3a7240c866a14af5594d0cc73263
Mekyle Hemani <153373890+Mekyle-Hemani@users.noreply.github.com>
2024-10-28 16:57:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,18 +12,18 @@ colourprint.print_colored(solution, colourprint.GREEN)
 
 #Establishing communication with microcontroller
 while True:
-    if "testing" in (SimpleSerial.SimpleSerialMain.readDataOnce()):
+    if "testing" in (SimpleSerial.SimpleSerialMain.readDataOnce(debug=1)):
         break
 while True:
     SimpleSerial.SimpleSerialMain.writeDataOnce("sending")
-    if "recieved" in (SimpleSerial.SimpleSerialMain.readDataOnce()):
+    if "recieved" in (SimpleSerial.SimpleSerialMain.readDataOnce(debug=1)):
         break
 colourprint.print_colored("Communication established", colourprint.BLUE)
 
 SimpleSerial.SimpleSerialMain.writeDataOnce(solution)
 
 while True:
-    if "testing" in (SimpleSerial.SimpleSerialMain.readDataOnce()):
+    if "testing" in (SimpleSerial.SimpleSerialMain.readDataOnce(debug=1)):
         break
 
 colourprint.print_colored(("Cube solved using "+solution), colourprint.GREEN)
