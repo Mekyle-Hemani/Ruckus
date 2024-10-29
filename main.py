de438fd86519ca7a78b92e1642cccdaa9d1dd95e
Mekyle Hemani <153373890+Mekyle-Hemani@users.noreply.github.com>
2024-10-28 20:14:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,12 +14,4 @@ colourprint.print_colored(solution, colourprint.GREEN)
 
 colourprint.print_colored("Communication to microcontroller established", colourprint.BLUE)
 
-ser = serial.Serial('COM4', 9600)
-data = "a"+solution
-try:
-    ser.write((data + "\n").encode())
-    time.sleep(1)
-except Exception as e:
-    print("Error sending data:", e)
-finally:
-    ser.close()
+SimpleSerial.SimpleSerialMain.writeDataOnce("a"+solution, debug=1)
