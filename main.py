8114b5efea3f28b832a28c58328a0d6592a31d42
Mekyle Hemani <153373890+Mekyle-Hemani@users.noreply.github.com>
2024-11-04 08:42:43 -0500

    Deleted un-needed files

--- main.py
+++ main.py
@@ -12,8 +12,6 @@ colourprint.print_colored(solution, colourprint.GREEN)
 
 colourprint.print_colored("Communication to microcontroller established", colourprint.BLUE)
 
-print("")
-
 if (SimpleSerial.SimpleSerialMain.writeDataOnce("a"+solution, debug=1) == "0"):
     colourprint.print_colored("Device has not recieved scramble", colourprint.RED)
     quit()
