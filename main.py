177e504beb3410a17a173cd274b08d9227cb3c5d
unknown <mhemani111@gmail.com>
2025-10-17 10:12:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ import visualizer
 cubeArray = []
 
 cubeArray=cubeStateManagement.resetCube()
-cubeArray=transformations.applyTransformations()
+cubeArray=transformations.applyTransformations(["f","r","u","r'","u'","f'"], cubeArray)
 print(cubeArray)
 visualizer.visualizeCube(cubeArray)
